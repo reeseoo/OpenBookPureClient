508febbbe1aaa85b089f0e35ac5a429c4a24c92a
Reese Morris <reesemorris@Reeses-MacBook-Pro.local>
2015-12-08 20:51:39 +0000

    Updates to material design

--- js/story.ctrl.js
+++ js/story.ctrl.js
@@ -1,12 +1,5 @@
 ﻿angular.module('OpenBook').controller("storyCtrl", function ($scope, $http) {
     var vm = this;
 
-    $http.get('http://openbookapi.azurewebsites.net/api/story').success(function (data, status, headers, config) {
-        alert("Got them stories bruh")
-        $scope.story = data;
-    }).error(function (data, status, headers, config) {
-        alert("Unable to retrieve story");
-    });
-
     vm.title = $scope.story;
 });
