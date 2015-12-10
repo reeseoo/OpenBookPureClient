747042305e4d2edbc44f1de5b7fe6977c84b02e7
Reese Morris <reesemorris@Reeses-MacBook-Pro.local>
2015-12-10 21:45:49 +0000

    Toggle div display

--- js/snippet.ctrl.js
+++ js/snippet.ctrl.js
@@ -9,7 +9,7 @@
     $scope.showSnippet = false;
     
     $scope.showDiv = function(){
-        $scope.showSnippet = true;
+        $scope.showSnippet = !$scope.showSnippet;
     }
 
     $http.get('http://openbookapi.azurewebsites.net/api/snippet').success(function (data, status, headers, config) {
