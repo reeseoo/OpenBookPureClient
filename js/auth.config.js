6e2e0047bb0a0fdb2c3cdc026fd048d668fe6803
John Bradshaw <johnbradshaw@Johns-MacBook-Pro.local>
2015-12-12 10:45:31 +0000

    stay logged in when you switch page

--- js/auth.config.js
+++ js/auth.config.js
@@ -3,6 +3,17 @@ openBookApp.config(function (authProvider) {
     domain: 'openbook.eu.auth0.com',
     clientID: '6Ax1d9qDK2k2OFT2iPu9JSvhdaEwaNXT'
   });
+}).config(function (authProvider, $routeProvider, $httpProvider, jwtInterceptorProvider) {
+  // ...
+
+  // We're annotating this function so that the `store` is injected correctly when this file is minified
+  jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+    // Return the saved token
+    return store.get('token');
+  }];
+
+  $httpProvider.interceptors.push('jwtInterceptor');
+  // ...
 })
 .run(function(auth) {
   // This hooks al auth events to check everything as soon as the app starts
--- js/site.js
+++ js/site.js
@@ -1,1 +1,16 @@
-﻿var openBookApp = angular.module('OpenBook', ['ngRoute', 'ngMaterial','auth0', 'angular-storage', 'angular-jwt'])
+﻿var openBookApp = angular.module('OpenBook', ['ngRoute', 'ngMaterial','auth0', 'angular-storage', 'angular-jwt']).run(function($rootScope, auth, store, jwtHelper, $location) {
+  // This events gets triggered on refresh or URL change
+  $rootScope.$on('$locationChangeStart', function() {
+    var token = store.get('token');
+    if (token) {
+      if (!jwtHelper.isTokenExpired(token)) {
+        if (!auth.isAuthenticated) {
+          auth.authenticate(store.get('profile'), token);
+        }
+      } else {
+        // Either show the login page or use the refresh token to get a new idToken
+        $location.path('/');
+      }
+    }
+  });
+});
