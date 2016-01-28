af11652d6690ba3b42306fe65a556084cbaa5e22
John Bradshaw <johnbradshaw@MacBook-Pro.local>
2016-01-28 21:58:30 +0000

    test api url

--- app/app.module.js
+++ app/app.module.js
@@ -1,9 +1,9 @@
 ﻿var openBookApp = angular.module('OpenBook', ['ui.router', 'ngMaterial','ngMessages','auth0', 'angular-storage', 'angular-jwt'])
-/* DEV */
+ 
+/*  DEV
 .constant('APIURL','http://localhost:5000/api/');
-/*
+Docker*//*
 .constant('APIURL','http://192.168.99.100:5000/api/');
-*/
-/* TEST 
+TEST */
 .constant('APIURL','http://openbookapi.azurewebsites.net/api/');
-*/
+/**/
